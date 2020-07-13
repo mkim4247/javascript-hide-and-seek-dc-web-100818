cbfc72c3312f0101dee2530d4264c565e6cc5414
Michael Kim <m.kim4247@gmail.com>
2020-07-13 15:44:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested div div div div.target  ');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+     parseInt(lis[i].innerHTML + n)
+  }
+}
+
+function deepestChild() {
+  
+}
